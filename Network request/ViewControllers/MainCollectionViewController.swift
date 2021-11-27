588b90246d166ecdf4d3d2cdbc12f42c2d26731d
Shalgin Stanislav <s.shalgin@gmail.com>
2021-11-27 11:55:28 +0300

    Add AlertController

--- Network request/ViewControllers/MainCollectionViewController.swift
+++ Network request/ViewControllers/MainCollectionViewController.swift
@@ -19,12 +19,12 @@ enum UserActions: String, CaseIterable {
 class MainCollectionViewController: UICollectionViewController {
     
     let userActions = UserActions.allCases
-
+    
     // MARK: UICollectionViewDataSource
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         userActions.count
     }
-
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserActionCell
         
@@ -32,8 +32,43 @@ class MainCollectionViewController: UICollectionViewController {
         
         return cell
     }
+    
+    // MARK: UICollectionViewDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    // MARK: Private Methods
+    private func succesAlert() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(
+                title: "Success",
+                message: "You can see the results in the Debug area",
+                preferredStyle: .alert
+            )
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+        }
+    }
+    
+    private func failedAlert() {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(
+                title: "Failed",
+                message: "You can see the results in the Debug area",
+                preferredStyle: .alert
+            )
+            
+            let okAction = UIAlertAction(title: "Ok", style: .default)
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+        }
+    }
 }
 
+// MARK: UICollectionViewDelegateFlowLayout
 extension MainCollectionViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
