2de2adb5d0bd34f8392739ad1a513b1fc4ca722f
Shalgin Stanislav <s.shalgin@gmail.com>
2021-11-27 13:11:20 +0300

    Image VC URL Session add (load a picture)

--- Network request/ViewControllers/MainCollectionViewController.swift
+++ Network request/ViewControllers/MainCollectionViewController.swift
@@ -7,6 +7,14 @@
 
 import UIKit
 
+enum Link: String {
+    case imageURL = "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg"
+    case exampleOne = "https://swiftbook.ru//wp-content/uploads/api/api_course"
+    case exampleTwo = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
+    case exampleThree = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+    case exampleFour = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
+}
+
 enum UserActions: String, CaseIterable {
     case downloadImage = "Download Image"
     case exampleOne = "Example one"
