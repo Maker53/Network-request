ba8374f9e1db128aad393c92a978d870e32b54d5
Shalgin Stanislav <s.shalgin@gmail.com>
2021-11-27 15:13:18 +0300

    Add JSON Model

---
+++ Network request/Course.swift
@@ -0,0 +1,13 @@
+//
+//  Course.swift
+//  Network request
+//
+//  Created by Станислав on 27.11.2021.
+//
+
+struct Course {
+    let name: String
+    let imageURL: String
+    let number_of_lessons: Int
+    let number_of_tests: Int
+}
